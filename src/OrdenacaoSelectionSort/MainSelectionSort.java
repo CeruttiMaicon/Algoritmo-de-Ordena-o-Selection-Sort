faa935b134f12ed2b4ed0e2cd969b7976c1c8e58
CeruttiMaicon <maicon.cerutti@univille.br>
2018-07-30 20:26:48 -0300

    Commit inicial, do algoritmo que conta interações de um algoritmo de ordenação, o SelectionSort

---
+++ src/OrdenacaoSelectionSort/MainSelectionSort.java
@@ -0,0 +1,48 @@
+package OrdenacaoSelectionSort;
+
+import java.io.IOException;
+
+public class MainSelectionSort {
+
+	public static void main(String[] args) {
+		
+		Popula p = new Popula();
+		
+		int[] vetor   	= 	new int[10];
+
+		
+		p.aleatorio(vetor);
+//		p.crescente(crescenteVar);
+//		p.decrescente(decrescenteVar);
+		
+		
+		for (int i = 0; i < vetor.length; i++ ){
+			if (i == 0){
+				System.out.print("Vetor inicial = ");
+			}
+			System.out.print(" [" + vetor[i] + "] ");
+			
+			if (i == vetor.length - 1){
+				System.out.println("");
+			}
+		}
+		
+		SelectionSort algoritmo = new SelectionSort();
+		SelectionSort algoritmo2 = new SelectionSort();
+		int c = algoritmo.metodo(vetor);
+		
+		
+		for (int i = 0; i < vetor.length; i++ ){
+			if (i == 0){
+				System.out.print("Vetor final =   ");
+			}
+			System.out.print(" [" + vetor[i] + "] ");
+			if (i == vetor.length - 1){
+				System.out.println(c);
+			}
+		}
+		
+		
+	}
+
+}
